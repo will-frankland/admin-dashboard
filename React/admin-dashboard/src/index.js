29a09f378741e490a23605e7a8860d6fa7f32554
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-19 14:48:23 -0500

    import css styling into app.js and index.js, configure initial title to h1 and add classname

--- React/admin-dashboard/src/index.js
+++ React/admin-dashboard/src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
-import { ReactDOM } from "react-dom";
-
+import './index.css';
 import App from './App';
 
-ReactDOM.render(<Appl />, document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+  document.getElementById('root'),
+  );
