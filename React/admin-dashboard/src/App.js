ed6ad6695b4800287140ed7e4dd584bf4c64dfa2
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-20 18:40:20 -0500

    add styling to dynamic sidebar code

--- React/admin-dashboard/src/App.js
+++ React/admin-dashboard/src/App.js
@@ -24,15 +24,18 @@ const App = () => {
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div>
+            <div className='w-72 fixed sidebar
+            dark:bg-secondary-dark-bg
+            bg-white'>
               Sidebar
             </div>
           ) : (
-            <div>
-              Sidebar w-0
+            <div className='w-0
+            dark:bg-secondary-dark-bg'>
+              Sidebar
             </div>
           )}
-        </div>
+        </div> 
       </BrowserRouter>
     </div>
   )
