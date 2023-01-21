e2e01fbfdab5715a3960c0c8f4ac58ba550bcaba
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-20 19:00:30 -0500

    add routes for each elemnt of project

--- React/admin-dashboard/src/App.js
+++ React/admin-dashboard/src/App.js
@@ -47,8 +47,30 @@ const App = () => {
 
           <div>
             <Routes>
-              <Route path='/'
-              element='ECommerce' />
+              {/* Dashboard */}
+              <Route path='/' element='ECommerce' />
+              <Route path='/ecommerce' element='ECommerce' />
+
+              {/* Pages */}
+              <Route path='/orders' element='Orders' />
+              <Route path='/employees' element='Employees' />
+              <Route path='/customers' element='Customers' />
+
+              {/* Apps */}
+              <Route path='/kanban' element='Kanban' />
+              <Route path='/editor' element='Editor' />
+              <Route path='/calendar' element='Calendar' />
+              <Route path='/color-picker' element='Color-picker' />
+
+              {/* Charts */}
+              <Route path='/line' element='Line' />
+              <Route path='/area' element='Area' />
+              <Route path='/bar' element='bar' />
+              <Route path='/pie' element='Pie' />
+              <Route path='/financial' element='Financial' />
+              <Route path='/color-mapping' element='Color-mapping' />
+              <Route path='/pyramid' element='Pyramid' />
+              <Route path='/stacked' element='Stacked' />
             </Routes>
           </div>
         </div> 
