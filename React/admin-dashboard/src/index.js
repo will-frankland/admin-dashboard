6a6cc52d3693807e1d8a400e069551f3894cd25a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-20 21:54:00 -0500

    import context provider to index.js and wrap App inside of CP

--- React/admin-dashboard/src/index.js
+++ React/admin-dashboard/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from "react-dom";
 
 import './index.css';
 import App from './App';
+import { ContextProvider } from './contexts/ContextProvider';
 
 ReactDOM.render(
-    <App />,
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
   document.getElementById('root'),
   );
