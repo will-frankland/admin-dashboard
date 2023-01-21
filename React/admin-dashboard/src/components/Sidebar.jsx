944552bc8abfe90f9c9e80ee0a7d1c55507c5677
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-20 21:57:28 -0500

    import context provider into sidebar.js and use in place of static value activeMenu

--- React/admin-dashboard/src/components/Sidebar.jsx
+++ React/admin-dashboard/src/components/Sidebar.jsx
@@ -4,9 +4,10 @@ import { SiShopware } from 'react-icons/si';
 import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 import { links } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext;
 
   const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2';
   const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
