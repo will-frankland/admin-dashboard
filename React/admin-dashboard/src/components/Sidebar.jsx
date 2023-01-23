73af353ef9af0e2cadd73c68858e7b246261bd05
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-23 08:56:32 -0600

    import components, data and dependables into Ecomm page

--- React/admin-dashboard/src/components/Sidebar.jsx
+++ React/admin-dashboard/src/components/Sidebar.jsx
@@ -34,7 +34,7 @@ const Sidebar = () => {
               onClick={() => setActiveMenu((prevActiveMenu) =>
                 !prevActiveMenu
               )}
-              className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 blockmd:hidden'
+              className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'
             >
               <MdOutlineCancel />
             </button>
@@ -43,8 +43,7 @@ const Sidebar = () => {
         <div className='mt-10'>
           { links.map((item) => (
             <div key={item.title}>
-              <p className='text-gray-400 m-3
-            mt-4 uppercase'>
+              <p className='text-gray-400 m-3 mt-4 uppercase'>
               {item.title}
               </p>
               {item.links.map((link) => (
