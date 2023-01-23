4af0e111284c44f4bc9c01863e9ee78b8310630a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-23 09:35:52 -0600

    add revenue updates with expense and budget points and styling

--- React/admin-dashboard/src/pages/Ecommerce.jsx
+++ React/admin-dashboard/src/pages/Ecommerce.jsx
@@ -31,9 +31,7 @@ const Ecommerce = () => {
 
         <div className='flex m-3 flex-wrap justify-center gap-1 items-center'>
           {earningData.map((item) => (
-            <div
-              key={item.title} className='bg-white dar:text-gray-200 dark:bg-secondary-dark-bg md:w-5 p-4 pt-9 rounded-2xl'
-              >
+            <div key={item.title} className='bg-white dark:text-gray-200 dark:bg-secondary-dark-bg md:w-5 p-4 pt-9 rounded-2xl'>
                 <button type='button'
                 style={{ color: item.iconColor, backgroundColor: item.iconBg}}
                 className='text-2xl opacity-0.9 rounded-full p-4 hover:drop-shadow-xl'>
@@ -43,15 +41,39 @@ const Ecommerce = () => {
                   <span className='text-lg font-semibold'>
                     {item.amount}
                   </span>
-                  <span className={`text-sm text-${item.pcColor} ml-2`}>
+                  <span className={`text-sm text${item.pcColor} ml-2`}>
                     {item.percentage}
                   </span>
+                </p>
+                <p className='text-sm text-gray-400 mt-1'>
+                  {item.title}
                 </p>
               </div>
           ))}
         </div>
       </div>
       
+      <div className='flex gap-10 flex-wrap justify-center'>
+            <div className='bg-white dark:text-gray-200
+            dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
+              <div className='flex justify-between'>
+                <p className='font-semi-bold text-xl'>Revenue Updates</p>
+                  <div className='flex items-center gap-4'>
+                    <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
+                      <span>
+                        <GoPrimitiveDot />
+                      </span>
+                      <span>Expense</span>
+                    </p>
+                    <p className='flex items-center gap-2 text-green-400 hover:drop-shadow-xl'></p>
+                    <span>
+                        <GoPrimitiveDot />
+                      </span>
+                      <span>Budget</span>
+                  </div>
+              </div>
+            </div>
+      </div>
     </div>
   )
 }
