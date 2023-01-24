1b51dfec0dec8eb77bebf795d97a48db845456ff
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-24 14:02:21 -0600

    add expense section to ecommerce page, utilizing <div> from budget

--- React/admin-dashboard/src/pages/Ecommerce.jsx
+++ React/admin-dashboard/src/pages/Ecommerce.jsx
@@ -78,10 +78,15 @@ const Ecommerce = () => {
                       <p>
                         <span className='text-3xl font-semibold'>$93,438</span>
                         <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>23%</span>
-
                       </p>
+                      <p className='text-gray-500 mt-1'>Budget</p>
                     </div>
-
+                    <div>
+                      <p>
+                        <span className='text-3xl font-semibold'>$48,438</span>
+                      </p>
+                      <p className='text-gray-500 mt-1'>Expense</p>
+                    </div>
                   </div>
                 </div>
             </div>
