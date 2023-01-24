4dba0b5d7f37149da668266206011597fc87797a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-24 14:00:08 -0600

    add $ amount as hardcoded value and introduce percentage symbol using tailwind styling

--- React/admin-dashboard/src/pages/Ecommerce.jsx
+++ React/admin-dashboard/src/pages/Ecommerce.jsx
@@ -71,7 +71,19 @@ const Ecommerce = () => {
                       <span>Budget</span>
                       </p>
                   </div>
-              </div>
+                </div>
+                <div className='mt-10 flex gap-10 flex-wrap justify-center'>
+                  <div className='border r-1 border-color m-4 pr-10'>
+                    <div>
+                      <p>
+                        <span className='text-3xl font-semibold'>$93,438</span>
+                        <span className='p-1.5 hover:drop-shadow-xl cursor-pointer rounded-full text-white bg-green-400 ml-3 text-xs'>23%</span>
+
+                      </p>
+                    </div>
+
+                  </div>
+                </div>
             </div>
       </div>
     </div>
