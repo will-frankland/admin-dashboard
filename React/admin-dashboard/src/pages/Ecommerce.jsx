2022b93dd131794c1df67af404e5558e82a8cbfa
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-24 13:52:40 -0600

    fix typo on 'semibold tailwind styling for Revenue Updates text

--- React/admin-dashboard/src/pages/Ecommerce.jsx
+++ React/admin-dashboard/src/pages/Ecommerce.jsx
@@ -56,7 +56,7 @@ const Ecommerce = () => {
             <div className='bg-white dark:text-gray-200
             dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780'>
               <div className='flex justify-between'>
-                <p className='font-semi-bold text-xl'>Revenue Updates</p>
+                <p className='font-semibold text-xl'>Revenue Updates</p>
                   <div className='flex items-center gap-4'>
                     <p className='flex items-center gap-2 text-gray-600 hover:drop-shadow-xl'>
                       <span>
