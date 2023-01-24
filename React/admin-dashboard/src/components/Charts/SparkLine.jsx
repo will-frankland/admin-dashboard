318d6b102c2b5e9b730334504f824f5028eccc37
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-24 14:13:01 -0600

    add props to Sparkline component to setup graph

--- React/admin-dashboard/src/components/Charts/SparkLine.jsx
+++ React/admin-dashboard/src/components/Charts/SparkLine.jsx
@@ -3,7 +3,14 @@ import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-re
 
 const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
   return (
-    <SparklineComponent>
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType='Numeric'
+      fill={color}
+      border={{ color: currentColor, width: 2 }}>
     
     </SparklineComponent>
   )
