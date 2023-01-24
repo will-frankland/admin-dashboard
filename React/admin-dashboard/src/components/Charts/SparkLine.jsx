7e1cc6fee1c33e1c96b65264d299a7cf9c95aee8
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-24 14:14:40 -0600

    add inject component to use SparkLineToolTip component services

--- React/admin-dashboard/src/components/Charts/SparkLine.jsx
+++ React/admin-dashboard/src/components/Charts/SparkLine.jsx
@@ -10,8 +10,9 @@ const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
       lineWidth={1}
       valueType='Numeric'
       fill={color}
-      border={{ color: currentColor, width: 2 }}>
-    
+      border={{ color: currentColor, width: 2 }}
+      >
+        <Inject services={[SparklineTooltip]} />
     </SparklineComponent>
   )
 }
