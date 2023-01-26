46e4bc5b4342e7a02e1ac327dea59129e8def76f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-26 14:49:20 -0600

    fix typo of tooltip on Stacked page and add height and width as props to Stacked component

--- React/admin-dashboard/src/components/Charts/Stacked.jsx
+++ React/admin-dashboard/src/components/Charts/Stacked.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooptip, Chart } from '@syncfusion/ej2-react-charts'
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip, Chart } from '@syncfusion/ej2-react-charts'
 import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy'
 import { ToolBase } from '@syncfusion/ej2/diagrams'
 
-const Stacked = () => {
+const Stacked = (width, height) => {
   return (
-    <ChartComponent>
+    <ChartComponent
+      width={width}
+      height={height}
+      >
       <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
     </ChartComponent>
   )
