6c75a4552e4c7efac790d31a9920e63f1f2bd742
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-26 15:25:48 -0600

    add braces to props of stacked - chart now showing Error fix

--- React/admin-dashboard/src/components/Charts/Stacked.jsx
+++ React/admin-dashboard/src/components/Charts/Stacked.jsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, StackingColumnSeries, Tooltip } from '@syncfusion/ej2-react-charts'
 import { stackedCustomSeries, stackedPrimaryXAxis, stackedPrimaryYAxis } from '../../data/dummy'
-import { ToolBase } from '@syncfusion/ej2/diagrams'
+import { useStateContext } from '../../contexts/ContextProvider';
 
-const Stacked = (width, height) => {
+const Stacked = ({width, height}) => {
+  const { currentMode } = useStateContext();
   return (
-    Stacked
-    // <ChartComponent
-    //   width={width}
-    //   height={height}
-    //   id="charts"
-    //   primaryXAxis={stackedPrimaryXAxis}
-    //   primaryYAxis={stackedPrimaryYAxis}
-    //   chartArea={{ border: { width: 0 }}}
-    //   tooltip={{ enable: true}}
-    //   legendSettings={{ background: 'white' }}
-    //   >
-    //   <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
-    //   <SeriesCollectionDirective>
-    //     {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {... item}/> )}
-    //   </SeriesCollectionDirective>
-    // </ChartComponent>
+    <ChartComponent
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      legendSettings={{ background: 'white' }}
+      >
+      <Inject services={[Legend, Category, StackingColumnSeries, Tooltip]} />
+      <SeriesCollectionDirective>
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item}/> )}
+      </SeriesCollectionDirective>
+    </ChartComponent>
   );
 };
 
