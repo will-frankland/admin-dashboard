c58f06e6180f5a030881312ae322c4d22bb84817
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-26 15:40:16 -0600

    add columnDirective and use spread operator on map function to create table and allow data to be written into individual columns

--- React/admin-dashboard/src/pages/Orders.jsx
+++ React/admin-dashboard/src/pages/Orders.jsx
@@ -8,6 +8,13 @@ const Orders = () => {
   return (
     <div className='margin-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header category='Page' title='Orders'/>
+      <GridComponent>
+        <ColumnsDirective>
+        {ordersGrid.map((item, index) => (
+          <ColumnDirective key={index} {...item} />
+        ))}
+        </ColumnsDirective>
+      </GridComponent>
     </div>
   )
 }
