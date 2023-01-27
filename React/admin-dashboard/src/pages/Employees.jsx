3dc57312abcca6dc3dfb060ed577f9709cedc487
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-27 15:40:23 -0600

    copy Orders page to Employees page and modify imports to suit Employees

--- React/admin-dashboard/src/pages/Employees.jsx
+++ React/admin-dashboard/src/pages/Employees.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject  } from '@syncfusion/ej2-react-grids';
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
+
 
 const Employees = () => {
   return (
-    <div>
-      Employees
+    <div className='margin-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category='Page' title='Orders'/>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+      >
+        <ColumnsDirective>
+        {ordersGrid.map((item, index) => (
+          <ColumnDirective key={index} {...item} />
+        ))}
+        </ColumnsDirective>
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
+      </GridComponent>
     </div>
   )
 }
 
-export default Employees
+export default Employees;
