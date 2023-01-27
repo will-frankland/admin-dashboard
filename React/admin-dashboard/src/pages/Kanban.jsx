b788d3c66e464d3fbbeeacbe20076c1b279fd68e
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-27 16:54:46 -0600

    add Kanban component and map over kanbanGrid data

--- React/admin-dashboard/src/pages/Kanban.jsx
+++ React/admin-dashboard/src/pages/Kanban.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/ej2-react-kanban';
-import { kanbanData, KanbanGrid } from '../data/dummy';
+import { kanbanData, kanbanGrid, KanbanGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Kanban = () => {
   return (
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
-      <Header category="App" title="Calendar"/>
+      <Header category="App" title="Kanban"/>
+      <KanbanComponent>
+        <ColumnsDirective>
+        {kanbanGrid.map((item, index) =>
+        <ColumnDirective key={index} {...item}/>
+        )}
+        </ColumnsDirective>
+      </KanbanComponent>
     </div>
   )
 }
