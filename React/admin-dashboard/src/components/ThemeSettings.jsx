a664289786f9145c09eb5f03fb81fc68fc8c1739
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-28 11:41:39 -0600

    add imports to themeSettings component

--- React/admin-dashboard/src/components/ThemeSettings.jsx
+++ React/admin-dashboard/src/components/ThemeSettings.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsCheck } from 'react-icons/bs';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { themeColors } from '../data/dummy';
+
+import { useStateContext } from '../contexts/ContextProvider';
 
 const ThemeSettings = () => {
   return (
@@ -8,4 +14,4 @@ const ThemeSettings = () => {
   )
 }
 
-export default ThemeSettings
+export default ThemeSettings;
