67a482112f993b8082008f9630e3ee47aca1260b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-28 11:16:25 -0600

    add inline pallette wording and styling

--- React/admin-dashboard/src/pages/ColorPicker.jsx
+++ React/admin-dashboard/src/pages/ColorPicker.jsx
@@ -5,7 +5,17 @@ import { Header } from '../components';
 const ColorPicker = () => {
   return (
     <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
-      <Header category="App" title="ColorPicker"/>
+      <Header category="App" title="Color Picker"/>
+        <div className='text-center'>
+          <div id='preview' />
+          <div className='flex justify-center items-center gap-20 flex-wrap'>
+            <div>
+              <p className='text-2xl font-semibold mt-2 mb-4'>Inline Pallette</p>
+            </div>
+
+          </div>
+
+        </div>
     </div>
   )
 }
