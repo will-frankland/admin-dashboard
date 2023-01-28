7c70b2c37ca40b606ee4c4471ba3146595fc0854
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-28 11:21:34 -0600

    add inline picker next to pallete

--- React/admin-dashboard/src/pages/ColorPicker.jsx
+++ React/admin-dashboard/src/pages/ColorPicker.jsx
@@ -19,6 +19,16 @@ const ColorPicker = () => {
                 showButtons={false}
                 />
             </div>
+            <div>
+              <p className='text-2xl font-semibold mt-2 mb-4'>Inline Picker</p>
+              <ColorPickerComponent
+                id='inline-pallete'
+                mode='Picker'
+                modeSwitcher={false}
+                inline
+                showButtons={false}
+                />
+            </div>
 
           </div>
 
