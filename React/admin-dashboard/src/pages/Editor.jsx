ff5bfe4a87a1651795e07b0177065d973698c332
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-28 11:01:24 -0600

    add import items to Editor

--- React/admin-dashboard/src/pages/Editor.jsx
+++ React/admin-dashboard/src/pages/Editor.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor';
+import { EditorData } from '../data/dummy';
+import { Header } from '../components';
+
 
 const Editor = () => {
   return (
