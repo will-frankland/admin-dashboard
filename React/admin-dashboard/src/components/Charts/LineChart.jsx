8fb8735099e1892b3aa1341d897a3a9d66dd30a9
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-29 12:40:37 -0500

    add imports to LineChart component and add LineChart component to Line.jsx to render

--- React/admin-dashboard/src/components/Charts/LineChart.jsx
+++ React/admin-dashboard/src/components/Charts/LineChart.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
+import { lineCustomSeries, LinePrimaryYAxis, linePrimaryXaxis } from '../../data/dummy';
 
 const LineChart = () => {
   return (
