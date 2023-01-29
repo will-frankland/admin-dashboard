da3bbdadc026342cb85da9d97953a202eb8f6ec4
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-29 11:35:05 -0500

    add dynamic code to navbar so that color updates when theme is selected instead of hardcoded 'blue'

--- React/admin-dashboard/src/components/Navbar.jsx
+++ React/admin-dashboard/src/components/Navbar.jsx
@@ -28,7 +28,7 @@ const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
 
 const Navbar = () => {
   const { activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick,
-  screenSize, setScreenSize } = useStateContext();
+  screenSize, setScreenSize, currentColor } = useStateContext();
 
   useEffect(() => {
     const handleResize = () => setScreenSize(window.innerWidth);
@@ -61,21 +61,21 @@ const Navbar = () => {
           <NavButton
             title="Cart"
             customFunc={() => handleClick('cart')}
-            color="blue"
+            color={currentColor}
             icon={<FiShoppingCart />}
           />
           <NavButton
             title="Chat"
             dotColor="#03C9D7"
             customFunc={() => handleClick('chat')}
-            color="blue"
+            color={currentColor}
             icon={<BsChatLeft />}
           />
           <NavButton
             title="Notifications"
             dotColor="#03C9D7"
             customFunc={() => handleClick('notification')}
-            color="blue"
+            color={currentColor}
             icon={<RiNotification3Line />}
           />
           <TooltipComponent
