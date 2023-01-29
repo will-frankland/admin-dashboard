19e34dfa810b9760c73ff5dd7da2b9ec68e32079
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-29 15:14:57 -0500

    add darkmode compatability to LineChart

--- React/admin-dashboard/src/components/Charts/LineChart.jsx
+++ React/admin-dashboard/src/components/Charts/LineChart.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip, LineSeries } from '@syncfusion/ej2-react-charts';
 import { lineCustomSeries, LinePrimaryYAxis, LinePrimaryXAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
 
 const LineChart = () => {
+  const { currentMode } = useStateContext();
   return (
     <ChartComponent
       id='line-chart'
@@ -11,6 +13,7 @@ const LineChart = () => {
       primaryYAxis={LinePrimaryYAxis}
       chartArea={{ border: { width: 0 }}}
       tooltip={{ enable: true }}
+      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
       >
       <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
       <SeriesCollectionDirective>
