b636133ae56d15982ac9c317456d49ee96354dd4
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-29 12:17:20 -0500

    add imports to Line chart component

--- React/admin-dashboard/src/pages/Charts/Line.jsx
+++ React/admin-dashboard/src/pages/Charts/Line.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from 'react';
+
+import { Header, LineChart } from '../../components';
 
 const Line = () => {
   return (
-    <div>
+    <div className='m-4 md: m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
       Line
     </div>
   )
