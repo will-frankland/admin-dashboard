4c14ab626b2939255e5966fdaf8cf418e859bd7a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-29 16:21:23 -0500

    update labels correctly in Bar, Area and Line chart

--- React/admin-dashboard/src/pages/Charts/Area.jsx
+++ React/admin-dashboard/src/pages/Charts/Area.jsx
@@ -8,7 +8,7 @@ const Area = () => {
   const { currentMode } = useStateContext();
   return (
     <div className='m-4 md: m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-      <Header category='Chart' title='Inflation Rate'/>
+      <Header category='Area' title='Inflation Rate in Percentage'/>
     <ChartComponent
       id='area-chart'
       height='420px'
--- React/admin-dashboard/src/pages/Charts/Bar.jsx
+++ React/admin-dashboard/src/pages/Charts/Bar.jsx
@@ -1,31 +1,48 @@
-import React from 'react';
-import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Category, Tooltip, Legend, ColumnSeries, DataLabel } from '@syncfusion/ej2-react-charts';
-import { barCustomSeries, barPrimaryXAxis, barPrimaryYAxis } from '../../data/dummy';
-import { useStateContext } from '../../contexts/ContextProvider';
-import { ChartsHeader } from '../../components';
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Category,
+  Tooltip,
+  Legend,
+  ColumnSeries,
+  DataLabel,
+} from "@syncfusion/ej2-react-charts";
+import {
+  barCustomSeries,
+  barPrimaryXAxis,
+  barPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Header } from "../../components";
 
 const Bar = () => {
   const { currentMode } = useStateContext();
   return (
-    <div className='m-4 md: m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-      <ChartsHeader category='Chart' title='Olympic Medal Counts: Rio'/>
-    <ChartComponent
-      id='bar-chart'
-      height='420px'
-      primaryXAxis={barPrimaryXAxis}
-      primaryYAxis={barPrimaryYAxis}
-      chartArea={{ border: { width: 0 }}}
-      tooltip={{ enable: true }}
-      background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+    <div className="m-4 md: m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category="Bar" title="Olympic Medal Counts: Rio" />
+      <ChartComponent
+        id="bar-chart"
+        height="420px"
+        primaryXAxis={barPrimaryXAxis}
+        primaryYAxis={barPrimaryYAxis}
+        chartArea={{ border: { width: 0 } }}
+        tooltip={{ enable: true }}
+        background={currentMode === "Dark" ? "#33373E" : "#fff"}
       >
-      <Inject services={[ ColumnSeries, Legend, Tooltip, Category, DataLabel ]} />
-      <SeriesCollectionDirective>
-        {barCustomSeries.map((item, index) =>
-        <SeriesDirective key={index} {...item} />)}
-      </SeriesCollectionDirective>
-    </ChartComponent>
-  </div>
-  )
+        <Inject
+          services={[ColumnSeries, Legend, Tooltip, Category, DataLabel]}
+        />
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
+    </div>
+  );
 };
 
 export default Bar;
