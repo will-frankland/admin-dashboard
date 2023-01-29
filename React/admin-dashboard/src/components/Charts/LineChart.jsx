95b4dffff36b99dc91089d10cdbab2d1e8c1ee14
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-29 15:11:11 -0500

    add tooltip to show % at each marker and add styling to chart

--- React/admin-dashboard/src/components/Charts/LineChart.jsx
+++ React/admin-dashboard/src/components/Charts/LineChart.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, DateTime, Legend, Tooltip, LineSeries } from '@syncfusion/ej2-react-charts';
-import { lineCustomSeries, LinePrimaryYAxis, linePrimaryXaxis } from '../../data/dummy';
+import { lineCustomSeries, LinePrimaryYAxis, LinePrimaryXAxis } from '../../data/dummy';
 
 const LineChart = () => {
   return (
-    <ChartComponent>
+    <ChartComponent
+      id='line-chart'
+      height='420px'
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{ border: { width: 0 }}}
+      tooltip={{ enable: true }}
+      >
       <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
       <SeriesCollectionDirective>
         {lineCustomSeries.map((item, index) =>
