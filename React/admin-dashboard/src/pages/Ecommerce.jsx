2a909ec0f6815e0bf1fee9b088493bfde082943b
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-29 11:43:52 -0500

    add stateContext to Ecommerce page and add dynamic code to modify theme when selected, hard coding color removed

--- React/admin-dashboard/src/pages/Ecommerce.jsx
+++ React/admin-dashboard/src/pages/Ecommerce.jsx
@@ -11,6 +11,7 @@ import {
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Ecommerce = () => {
+  const { currentColor } = useStateContext();
   return (
     <div className="mt-24">
       <div className="flex flex-wrap lg:flex-nowrap justify-center">
@@ -24,7 +25,7 @@ const Ecommerce = () => {
           <div className="mt-6">
             <Button
               color="white"
-              bgColor="blue"
+              bgColor={currentColor}
               text="Download"
               borderRadius="10px"
               size="md"
@@ -98,19 +99,19 @@ const Ecommerce = () => {
               </div>
               <div className="mt-5">
                 <SparkLine
-                  currentColor='blue'
+                  currentColor={currentColor}
                   id='line-sparkLine'
                   type='Line'
                   height='80px'
                   width='250px'
                   data={SparklineAreaData}
-                  color='blue'
+                  color={currentColor}
                    />
               </div>
               <div className='mt-10'>
                 <Button 
                   color='white'
-                  bgColor='blue'
+                  bgColor={currentColor}
                   text='Download Report'
                   borderRadius='10px'/>
               </div>
