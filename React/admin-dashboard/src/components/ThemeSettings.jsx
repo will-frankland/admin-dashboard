e3e76af4cb5b3ebdd12604c2cae927225c4bc11d
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-28 19:31:06 -0500

    add setColor onClick functionality and only display check mark on selected color

--- React/admin-dashboard/src/components/ThemeSettings.jsx
+++ React/admin-dashboard/src/components/ThemeSettings.jsx
@@ -69,10 +69,10 @@ const ThemeSettings = () => {
                     type='button'
                     className='h-10 w-10 rounded-full cursor-pointer'
                     style={{ backgroundColor: item.color }}
-                    onClick={() => {}}>
+                    onClick={() => setColor(item.color)}>
                     <BsCheck
-                      className={`ml-2 text-2xl text-white ${
-                        false ? "block" : "hidden"
+                      className={`ml-2 text-2xl text-white
+                        ${item.color === currentColor ? "block" : "hidden"
                       }`}
                     />
                   </button>
