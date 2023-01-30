6d41e3ada5f357a77510393442edffbb4b875af0
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-30 15:31:01 -0500

    bug fix - change Sparkline to class based component to remove bug where adjusting screensize caused webpage to go blank

--- React/admin-dashboard/src/components/Charts/SparkLine.jsx
+++ React/admin-dashboard/src/components/Charts/SparkLine.jsx
@@ -1,32 +1,40 @@
-import React from 'react'
-import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
+import React from "react";
+import {
+  SparklineComponent,
+  Inject,
+  SparklineTooltip,
+} from "@syncfusion/ej2-react-charts";
 
-const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
-  return (
-    <SparklineComponent
-      id={id}
-      height={height}
-      width={width}
-      lineWidth={1}
-      valueType='Numeric'
-      fill={color}
-      border={{ color: currentColor, width: 2 }}
-      dataSource={data}
-      xName='x'
-      yName='yval'
-      type={type}
-      tooltipSettings={{
-        visible: true,
-        format: '${x} : data ${yval}',
-        trackLineSettings: {
+class SparkLine extends React.PureComponent {
+  render() {
+    const { id, height, width, color, data, type, currentColor } = this.props;
+
+    return (
+      <SparklineComponent
+        id={id}
+        height={height}
+        width={width}
+        lineWidth={1}
+        valueType="Numeric"
+        fill={color}
+        border={{ color: currentColor, width: 2 }}
+        dataSource={data}
+        xName="x"
+        yName="yval"
+        type={type}
+        tooltipSettings={{
           visible: true,
-        },
-      }}
-      markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+          format: "${x} : data ${yval}",
+          trackLineSettings: {
+            visible: true,
+          },
+        }}
+        markerSettings={{ visible: ["All"], size: 2.5, fill: currentColor }}
       >
         <Inject services={[SparklineTooltip]} />
-    </SparklineComponent>
-  );
-}
+      </SparklineComponent>
+    );
+  }
+};
 
 export default SparkLine;
--- React/admin-dashboard/src/pages/Charts/ColorMapping.jsx
+++ React/admin-dashboard/src/pages/Charts/ColorMapping.jsx
@@ -10,7 +10,7 @@ const ColorMapping = () => {
 
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
-      <Header category="Color Mappping" title="USA CLIMATE - WEATHER BY MONTH" />
+      <Header category="Color Mappping" title="Climate in USA by Month (Avergae)" />
       <div className="w-full">
         <ChartComponent
           id="charts"
