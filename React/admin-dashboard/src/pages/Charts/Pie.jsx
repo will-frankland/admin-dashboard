9f161b7a4160cfa47115d1a1e3383e66e66ea296
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-01-30 15:05:56 -0500

    add pyramid and stacked chart code modifying previously utilized LineChart code

--- React/admin-dashboard/src/pages/Charts/Pie.jsx
+++ React/admin-dashboard/src/pages/Charts/Pie.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import { pieChartData } from "../../data/dummy";
+import { Header, Pie as PieChart } from "../../components";
 
-const Pie = () => {
-  return (
-    <div>
-      Pie
+const Pie = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <Header category="Pie" title="Project Cost Breakdown" />
+    <div className="w-full">
+      <PieChart
+        id="chart-pie"
+        data={pieChartData}
+        legendVisibility
+        height="full"
+      />
     </div>
-  )
-}
+  </div>
+);
 
-export default Pie
+export default Pie;
